26c20630ea9f134ae8d7fd631db1b893097a364c
Sahil Rzayev <sahilrzayev2000@gmail.com>
2021-07-06 09:37:50 +0400

    update employer detail

--- src/pages/Employee/EmployeeDetail.jsx
+++ src/pages/Employee/EmployeeDetail.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Card, Image } from "semantic-ui-react";
+import { Card, Divider, Grid, GridColumn, Image } from "semantic-ui-react";
 import EmployeeService from "../../services/employeeService";
 import EmployeeUpdate from "./EmployeeUpdate";
 
@@ -13,16 +13,39 @@ export default function EmployeeDetail() {
 
   return (
     <div>
-      <div style={{ position: "relative", marginLeft: "500px" }}>
-        <Image src={"../../../assets/avataremployee.jpg"} size="medium" />
-        <Card
-          header={employee?.firstName}
-          meta={employee?.lastName}
-          description={employee?.email}
-        />
-        <div style={{ float: "left" }}>
+      <div style={{ position: "relative", marginLeft: "200px",marginTop:"10em" }}>
+        {/* <Image src={"../../../assets/avataremployee.jpg"} size="medium" /> style={{marginTop:"6px"}}*/}
+        <Card style={{fontSize:"20px",width:"1000px",height:"300px"}}>
+        <span style={{fontSize:"25px",marginRight:"30em",marginTop:"0.5em",color:"purple"}}>Özel Bilgiler</span><Divider/>
+        <div style={{ float: "right",marginRight:"1em",marginTop:"-2.8em" }}>
           <EmployeeUpdate employee={employee} />
         </div>
+        <Grid columns={2}>
+        <Grid.Row textAlign="center">
+          <Grid.Column style={{marginTop:"2em"}}>
+        <Card.Meta>
+        <span className='employeeStyle1'>İsim</span>
+        </Card.Meta>
+        <Card.Header className="employeeStyle">{employee?.firstName}</Card.Header>
+        </Grid.Column>
+        <Grid.Column style={{marginTop:"2em"}}>
+        <Card.Meta>
+        <span className='employeeStyle1'>Soyisim</span>
+        </Card.Meta>
+        <Card.Header className="employeeStyle">{employee?.lastName}</Card.Header>
+        </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+        <Grid.Column style={{marginTop:"2em"}}>
+        <Card.Meta>
+        <span className='employeeStyle1'>Email</span>
+        </Card.Meta>
+        <Card.Header className="employeeStyle">{employee?.email}</Card.Header>
+        </Grid.Column>
+        </Grid.Row>
+        </Grid>
+        </Card>
+        
       </div>
     </div>
   );
