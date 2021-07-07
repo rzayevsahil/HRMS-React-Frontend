1137072f38f033a0a14d4aa505cbeaa64928b35c
Sahil Rzayev <sahilrzayev2000@gmail.com>
2021-07-07 18:52:20 +0400

    sign in updated

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,6 +1,10 @@
+import { useFormik } from "formik";
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { Button, Form, Grid, Header, Icon, Image, Message, Segment } from 'semantic-ui-react'
+import UserService from "../services/userService";
+import * as Yup from "yup";
+import swal from "sweetalert";
 
 export default function SignIn() {
 
@@ -9,6 +13,42 @@ export default function SignIn() {
   function visibles(){
     setVisible(!visible);
   }
+
+  const jobSeekerRegisterSchema = Yup.object().shape({
+    email: Yup.string().required("Email alanı zorunludur").email("Geçerli bir email değil"),
+    // password: Yup.string().required("Şifre zorunludur").min(8,"Şifre en az 8 karakter uzunlugunda olmalıdır")
+  });
+
+  const history = useHistory();
+
+  const formik= useFormik({
+    initialValues: {
+      email:"",
+      password:""
+    },
+    validationSchema: jobSeekerRegisterSchema,
+    onSubmit:(values) => {
+      console.log(values)
+      let userService = new UserService();
+      userService.findByEmailAndPassword(values.email,values.password).then((result) =>  {
+        console.log(result);
+      if (result.data.success===false) {
+       return swal(
+       {
+        title: result.data.message,
+        text: "",
+        icon: "warning",
+        dangerMode: true,
+        button:"Kapat"
+      })
+      }else{
+        swal("Başarıyla giriş yapıldı","","success");
+        history.push("/signIn")
+
+      }})
+      
+    }
+  });
   return (
     <div style={{marginLeft:"15%",marginTop:"-7%",backgroundColor:("../../assets/logo.png")}}>
     
@@ -17,9 +57,25 @@ export default function SignIn() {
       <Header as='h2' color='violet' textAlign='center'>
         <Image src='../../assets/logo.png' /> Giriş Yap
       </Header>
-      <Form size='large'>
+      <Form size='large' onSubmit={formik.handleSubmit}>
         <Segment stacked>
-          <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail' />
+          {/* <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail' /> */}
+          <Form.Input
+                fluid
+                icon="mail"
+                iconPosition="left"
+                placeholder="E-mail adresi"
+                type="email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                name="email"
+              />
+              {formik.errors.email && formik.touched.email && (
+                  <div className={"ui pointing red basic label"}>
+                    {formik.errors.email}
+                  </div>
+                )}
         
           
           {visible?(
@@ -31,20 +87,36 @@ export default function SignIn() {
             placeholder='Password'
             type='password'
             style={{position:"relative",zIndex:1}}
-          />):( <Form.Input
+            value={formik.values.password}
+            name="password"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+          ):( <Form.Input
             fluid
             icon='lock'
             iconPosition='left'
             placeholder='Password'
             type='text'
-          />)}
+            value={formik.values.password}
+            name="password"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+          )
+          }
+          {formik.errors.password && formik.touched.password && (
+                  <div className={"ui pointing red basic label"}>
+                    {formik.errors.password}
+                  </div>
+                )}
           {visible?
           (<Icon style={{marginTop:"-40px",marginBottom:"5px",marginLeft:"-105px",marginRight:"10px",float:"right",position:"relative",zIndex:3}} name="eye" onClick={visibles}/>):
           (<Icon style={{marginTop:"-40px",marginBottom:"5px",marginLeft:"-105px",marginRight:"10px",float:"right",position:"relative",zIndex:3}} name="eye slash" onClick={visibles}/>)}
           
          
 
-          <Button color='violet' fluid size='large'>
+          <Button color='violet' fluid size='large' type="submit">
             Login
           </Button>
         </Segment>
--- src/services/userService.js
+++ src/services/userService.js
@@ -10,4 +10,13 @@ export default class UserService{
     countGetAll(){
         return axios.get("http://localhost:8080/api/users/countGetAll");
     }
+
+    signIn(email,password){
+        return axios.get("http://localhost:8080/api/users/signIn?email="+email+"&password="+password);
+    }
+
+    findByEmailAndPassword(email,password){
+        return axios.get("http://localhost:8080/api/users/findByEmailAndPassword?email="+email+"&password="+password);
+    }
+    
 }
